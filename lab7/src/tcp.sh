1c6dae5e4e041f98208123392e57b7813083e988
Yash Kin <girokompass@gmail.com>
2013-11-08 22:23:56 +0300

    bash stuff

--- lab7/src/tcp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-(ruby main.rb -g 127.0.0.1 -p 5555 -f 1.mp3;
- ruby main.rb -g 127.0.0.1 -p 5555 -f 2.mp3;
- ruby main.rb -g 127.0.0.1 -p 5555 -f 3.mp3) | parallel
-
-#parallel sh -c "ruby main.rb -g 127.0.0.1 -p 5555 -f i.mp3" -- 1 2 3 4 5
